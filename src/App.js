36a60363136bbee035302e963700b31d539e70c5
Daniel Levelev <levelev@gmail.com>
2017-12-16 15:14:56 +0100

    grid, flats, props

--- src/App.js
+++ src/App.js
@@ -4,20 +4,40 @@ import './App.css';
 import Flat from './components/flat';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    }
+  }
   render() {
+    // const flat = {
+    //   "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+    //   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+    //   "price": 164,
+    //   "priceCurrency": "EUR",
+    //   "lat": 48.884211,
+    //   "lng": 2.346890
+    // };
 
-    const flat = {
-      "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-      "price": 164,
-      "priceCurrency": "EUR",
-      "lat": 48.884211,
-      "lng": 2.346890
-    };
+    // const flats = [flat, flat, flat, flat];
 
     return (
-      <div>
-      <Flat flat={flat} />
+      <div className="app">
+        <div className="main">
+         <div className="search"></div>
+
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+
+            })}
+
+          </div>
+        </div>
+
+        <div className="map">
+        </div>
       </div>
       );
   }
