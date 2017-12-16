8f2c33e58be78573c10b01cf1286e3629d41df32
Daniel Levelev <levelev@gmail.com>
2017-12-16 15:29:34 +0100

    complete flats

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ class App extends React.Component {
     super(props);
     this.state = {
       flats: []
-    }
+    };
   }
+
+  componentDidMount () {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)  // AJAX
+      .then(response => response.json())
+      .then((data)  => {
+        this.setState({
+        flats: data
+        });
+      })
+  }
+
   render() {
-    // const flat = {
-    //   "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-    //   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-    //   "price": 164,
-    //   "priceCurrency": "EUR",
-    //   "lat": 48.884211,
-    //   "lng": 2.346890
-    // };
-
-    // const flats = [flat, flat, flat, flat];
-
     return (
       <div className="app">
         <div className="main">
